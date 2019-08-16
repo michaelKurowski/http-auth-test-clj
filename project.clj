0d12a0ab366c06d03132b51ac84ece418b558ae8
michaelKurowski <Byamarro@gmail.com>
2019-08-16 04:46:35 +0200

    working hello world over http

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
     [org.clojure/clojure "1.10.0"]
-    [http-kit "2.2.0"]
+    [http-kit "2.3.0"]
   ]
   :repl-options {:init-ns http-auth.core}
   :main http-auth.core)
--- src/http_auth/core.clj
+++ src/http_auth/core.clj
@@ -1,13 +1,11 @@
-(ns http-auth.core)
-;  (:require [org.httpkit.server :refer [run-server]]
-;    [clj-time.core :as t]))
+(ns http-auth.core
+  (:require [org.httpkit.server :refer [run-server]]))
 
 (defn app [req]
   {:status  200
       :headers {"Content-Type" "text/html"}
-      :body    "aa"})
+      :body    "Hello world"})
 
 (defn -main [& args]
-  (println "aaaa"))
-;  (run-server app {:port 8080})
-;  (println "Server started on port 8080"))
+  (run-server app {:port 8080})
+  (println "Server started on port 8080"))
